0dca2156470a65f9477e87acb500dce2208f60d5
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-03-16 05:36:56 +0100

    More functions

---
+++ src/init_maze.c
@@ -0,0 +1,14 @@
+#include "maze.h"
+
+/**
+ * init - Initializes SDL
+ *
+ * Description: Calls the SDL initialization function,
+ *		creates the game window and surface
+ * Return: true (boolean) if the initialization was successful, otherwise false
+ */
+
+bool init(void)
+{
+	return (true);
+}
