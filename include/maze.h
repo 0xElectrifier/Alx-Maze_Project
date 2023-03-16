30c193e4983d9a92524014cc66c4af0ec636b913
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-03-16 04:10:41 +0100

    Modified `.gitignore` file, deleted trash files and updated `maze.h` header file

--- include/maze.h
+++ include/maze.h
@@ -1,5 +1,7 @@
 /** Header file containing prototypes of functions I wrote
  * and includes of external libraries used in this project
  */
+
+#include <SDL/SDL2.h>
 #include <stdio.h>
-#include <SDL/sdl2.h>
+#include <stdbool.h>
