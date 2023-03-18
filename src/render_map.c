c10e280e74aeb601a1006c61ff31ae9cae557eca
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-03-18 10:06:46 +0100

    Refactored bulky code

--- src/render_map.c
+++ src/render_map.c
@@ -1,117 +1,162 @@
 #include "maze.h"
 
 
-double posX = 22, posY = 12; /* x and y start position */
-double dirX = -1, dirY = 0; /* Initial direction vector */
-double planeX = 0, planeY = 0.66; /*The 2d raycaster version of camera plane*/
-double time = 0; /* Time of current frame */
-double oldTime = 0; /* Time of previous frame */
+/**
+ * ray_player_data - initializes and assigns values to player_data and ray_data
+ * @p_data: PLAYER_DATA structure
+ * @r_data: RAY_DATA structure
+ *
+ * Return: 0 on success, otherwise 1
+ */
+int ray_player_data(PLAYER_DATA *p_data, RAY_DATA *r_data)
+{
+	/* Calculate ray position and direction */
+	(r_data)->cameraX = ((2 * x) / double(map_width)) - 1;
+	(r_data)->rayDirX = (p_data)->dirX + (p_data)->planeX * (r_data)->cameraX;
+	(r_data)->rayDirY = (p_data)->dirY + (p_data)->planeY * (r_data)->cameraX;
 
-double cameraX; /* x-coordinate in camera space */
-double rayDirX, rayDirY; /* The ray direction on the X and Y component */
-int x;
-SDL_Event e;
-bool quit = false;
-int mapX, mapY;
-/* Length of ray from current position to next x or y-side */
-double sideDistX, sideDistY;
-/* Length of ray from one x or y-side to next x or y-side *
-double deltaDistX, deltaDistY;*/
-double perpWallDist;
-/* What direction to step in x or y-direction (either +1 or -1) */
-int stepX, stepY;
-int hit; /* Was a wall hit? */
-int side; /* was a NS or a EW wall hit? */
+	/* Which box of the map player is in */
+	(r_data)->mapX = (p_data)->((int)posX);
+	(r_data)->mapY = (p_data)->((int)posY);
 
+	(r_data)->deltaDistX = (r_data)->rayDirX == 0 ? 1e30 : fabs(1 / r_data->rayDirX);
+	(r_data)->deltaDistY = (r_data)->rayDirY == 0 ? 1e30 : fabs(1 / r_data->rayDirY);
+
+	/* Calculate step and initial sideDist */
+
+	if ((r_data)->rayDirX < 0)
+	{
+		(r_data)->stepX = -1;
+		(r_data)->sideDistX = ((p_data)->posX - (r_data)->mapX) * (r_data)->deltaDistX;
+	}
+	else
+	{
+		(r_data)->stepX = 1;
+		(r_data)->sideDistX = (r_data)->mapX + 1.0 - (p_data)->posX;
+		(r_data)->sideDistX *= (r_data)->deltaDistX;
+	}
+	if ((r_data)->rayDirY < 0)
+	{
+		(r_data)->stepY = -1;
+		(r_data)->sideDistY = ((p_data)->posY - (r_data)->mapY) * (r_data)->deltaDistY;
+	}
+	else
+	{
+		(r_data)->stepY = 1;
+		(r_data)->sideDistY = ((r_data)->mapY + 1.0 - (p_data)->posY) * (r_data)->deltaDistY;
+	}
+
+	return (0);
+}
 
 /**
- * iterate_screenWidth - iterates through the
+ * iterate_screen_width - iterates through the vertical components of the screen
+ * @rc_data: RAYCAST_DATA struct
+ * @p_data: PLAYER_DATA struct
+ * @r_data: RAY_DATA struct
+ * @t_data: TIMING_DATA struct
+ *
+ * Return: 0 on successful iteration, otherwise 1
+ */
+int iterate_screen_width(RAYCAST_DATA *rc_data, PLAYER_DATA *p_data,
+			 RAY_DATA *r_data, TIMING_DATA *t_data)
+{
+	int x;
+
+	for (x = 0; x < map_width; x++)
+	{
+		/* Calculate ray position and direction */
+		ray_player_data(p_data, r_data);
+
+		/* Perform DDA */
+		(r_data)->hit = 0;
+		while ((r_data)->hit == 0)
+		{
+			/* Jump to next map square, either in x-direction, or in y-direction */
+			if ((r_data)->sideDistX < (r_data)->sideDistY)
+			{
+				(r_data)->sideDistX += (r_data)->deltaDistX;
+				(r_data)->mapX += (r_data)->stepX;
+				(r_data)->side = 0;
+			}
+			else
+			{
+				r_data->sideDistY += r_data->deltaDistY;
+				r_data->mapY += r_data->stepY;
+				r_data->side = 1;
+			}
+			/* Check if ray has hit a wall */
+			if (world_map[mapX][mapY] > 0)
+				hit = 1;
+		}
+
+		perpWallDist = (r_data->side == 0) ? (r_data->sideDistX - r_data->deltaDistX) : (r_data->sideDistY - r_data->deltaDistY);
+	}
+}
+
 /**
  * start_game_loop - handles game loop
- *
+ * @rc_data: RAYCAST_DATA struct
+ * @p_data: PLAYER_DATA struct
+ * @r_data: RAY_DATA struct
+ * @t_data: TIMING_DATA struct
  *
  * Description: this function begins the game loop, iterating through
  *		the vertical axis of the screen so images can be rendered.
  * Return: 
  */
 
-int start_game_loop(void)
+/**
+ * double posX = 22, posY = 12;
+ * double dirX = -1, dirY = 0;
+ * double planeX = 0, planeY = 0.66;
+ * double time = 0, oldTime = 0;
+ */
+
+int start_game_loop(RAYCAST_DATA *rc_data, PLAYER_DATA *p_data,
+		    RAY_DATA *r_data, TIMING_DATA *t_data)
 {
-	/* Game Loop start */
+	bool quit = false;
+
+	/* Stop loop when a defined user input is detected */
 	while (SDL_PollEvent(&e))
 	{
 		if (e.type == SDL_QUIT)
 			quit = true;
 
-		for (x = 0; x < map_width; x++)
-		{
-			/* Calculate ray position and direction */
-			cameraX = ((2 * x) / double(w)) - 1;
-			rayDirX = dirX + planeX * cameraX;
-			rayDirY = dirY + planeY * cameraX;
-
-			/* Which box of the map player is in */
-			mapX = (int)posX;
-			mapY = (int)posY;
-
-			double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-			double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-			hit = 0;
-
-			/* Calculate step and initial sideDist */
-			if (rayDirX < 0)
-			{
-				stepX = -1;
-				sideDistX = (posX - mapX) * deltaDistX;
-			}
-			else
-			{
-				stepX = 1;
-				sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-			}
-			if (rayDirY < 0)
-			{
-				stepY = -1;
-				sideDistY = (posY - mapY) * deltaDistY;
-			}
-			else
-			{
-				stepY = 1;
-				sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-			}
-			/* Perform DDA */
-
-			/* perform_dda(
-			 */
-			while (hit == 0)
-			{
-				/* Jump to next map square, either in x-direction, or in y-direction */
-				if (sideDistX < sideDistY)
-				{
-					sideDistX += deltaDistX;
-					mapX += stepX;
-					side = 0;
-				}
-				else
-				{
-					sideDistY += deltaDistY;
-					mapY += stepY;
-					side = 1;
-				}
-				/* Check if ray has hit a wall */
-				if (world_map[mapX][mapY] > 0)
-					hit = 1;
-			}
-			/* Calculate distance projected on camera direction
-			 * (Euclidean distance would give fisheye effect!)
-			 * set_perpWallDist
-			 */
-			perpWallDist = (side == 0) ? (sideDistX - deltaDistX) : (sideDistY - deltaDistY);
-		}
+		iterate_screen_width(rc_data, p_data, r_data, t_data);
 
 		if (quit == true)
 			break;
 	}
+
+	return (0);
+}
+
+/**
+ * game_loop - handles game loop
+ *
+ * Return:
+ */
+int game_loop(void)
+{
+	RAYCAST_DATA *rc_data;
+
+	rc_data = malloc(sizeof(RAYCAST_DATA));
+	if (rc_data == NULL)
+		return (1);
+	rc_data->player_data = malloc(sizeof(PLAYER_DATA));
+	if (rc_data->player_data == NULL)
+		return (1);
+	rc_data->ray_data = malloc(sizeof(RAY_DATA));
+	if (rc_data->ray_data == NULL)
+		return (1);
+	rc_data->timing_data = malloc(sizeof(TIMING_DATA));
+	if (rc_data->timing_data == NULL)
+		return (1);
+
+	start_game_loop(rc_data, rc_data->player_data,
+			rc_data->ray_data, rc_data->timing_data);
 
 	return (0);
 }
