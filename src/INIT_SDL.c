ba52ac64e7eb18c07653e0ed4474812bd8f0f436
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-03-25 11:11:01 +0100

    Contains functions that does the initial SDL initialization, including the window creation and so on

--- src/INIT_SDL.c
+++ src/INIT_SDL.c
@@ -1,5 +1,46 @@
 #include "maze.h"
 
+
+/**
+ * create_game_window - a singleton function that returns a
+ *			static GAME_WINDOW object
+ *
+ * Return: pointer to a static GAME_WINDOW object, otherwise NULL on failure
+ */
+GAME_WINDOW *create_game_window(void)
+{
+	static GAME_WINDOW *game_window = NULL;
+	static SDL_Window *window = NULL;
+	static SDL_Surface *screen_surface = NULL;
+
+	game_window = malloc(sizeof(GAME_WINDOW));
+	if (game_window == NULL)
+		return (NULL);
+
+	/* Create Window */
+	window = SDL_CreateWindow("Electrifier Maze", SDL_WINDOWPOS_UNDEFINED,
+				  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+				  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	/* On failure... */
+	if (window == NULL)
+	{
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return (NULL);
+	}
+	else
+	{
+		screen_surface = SDL_GetWindowSurface(window);
+		if (screen_surface == NULL)
+			return (NULL);
+
+	}
+	game_window->window = window;
+	game_window->screen_surface = screen_surface;
+
+	return (game_window);
+}
+
+
 /**
  * init - Initializes SDL
  *
@@ -8,13 +49,10 @@
  * Return: true (boolean) if the initialization was successful, otherwise false
  */
 
-bool init(void)
+GAME_WINDOW *init(void)
 {
-	SDL_Window *window = NULL;
-	SDL_Surface *screen_surface = NULL;
-	SDL_Event e;
-	(void) screen_surface;
 	bool success = true;
+	GAME_WINDOW *game_window = NULL;
 
 	/* Initializing SDL */
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -25,22 +63,10 @@ bool init(void)
 	}
 	else
 	{
-		/* Create Window */
-		window = SDL_CreateWindow("Electrifier Maze", SDL_WINDOWPOS_UNDEFINED,
-					  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-					  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		/* On failure... */
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		game_window = create_game_window();
+		if (game_window == NULL)
 			success = false;
-		}
-		else
-		{
-			screen_surface = SDL_GetWindowSurface(window);
-
-		}
 	}
 
-	return (success);
+	return (game_window);
 }
